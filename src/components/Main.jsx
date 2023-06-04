7ea8b8e9954420e50b2ebebdc144abc3b1df3c21
Helitha Rupasinghe <hr21don>
2023-06-04 12:14:02 +0100

    updated main.jsx with a carousel

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,7 @@ import requests from "../Requests";
 import axios from "axios";
 const Main = () => {
   const [movies, setMovies] = React.useState([]);
-
-  const movie = movies[Math.floor(Math.random() * movies.length)];
+  const [activeIndex, setActiveIndex] = React.useState(0);
 
   React.useEffect(() => {
     axios.get(requests.requestPopular).then((res) => {
@@ -12,7 +11,19 @@ const Main = () => {
     });
   }, []);
 
-  // console.log(movie);
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setActiveIndex((prevIndex) =>
+        prevIndex === movies.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [activeIndex, movies.length]);
+
+  const movie = movies[activeIndex];
 
   const truncateStr = (str, num) => {
     if (str?.length > num) {
@@ -22,8 +33,12 @@ const Main = () => {
     }
   };
 
+  const handleIndicatorClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
-    <div className="w-full h-[550px] text-white">
+    <div className="w-full h-[550px] text-white relative">
       <div className="w-full h-full">
         <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
         <img
@@ -31,21 +46,32 @@ const Main = () => {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-      </div>
-      <div className="absolute w-full top-[20%] p-4 md:p-8">
-        <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
-        <div className="my-4">
-          <button className="border bg-white  font-bold text-black  border-gray-300 py-2 px-5 hover:bg-white/50 hover:text-white">
-            Play
-          </button>
-          <button className="border text-white font-bold border-gray-300 py-2 px-5 ml-4">
-            Watch Later
-          </button>
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="my-4">
+            <button className="border bg-white  font-bold text-black  border-gray-300 py-2 px-5 hover:bg-white/50 hover:text-white">
+              Play
+            </button>
+            <button className="border text-white font-bold border-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
+          <p className="text-gray-400 text-sm">Released: {movie?.release_date}</p>
+          <p className="w-full mt-4 md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {truncateStr(movie?.overview, 190)}
+          </p>
         </div>
-        <p className="text-gray-400 text-sm">Released: {movie?.release_date}</p>
-        <p className="w-full mt-4 md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
-          {truncateStr(movie?.overview, 190)}
-        </p>
+      </div>
+      <div className="absolute left-0 right-0 bottom-2 flex justify-center">
+        {movies.map((m, index) => (
+          <div
+            key={m.id}
+            className={`w-2 h-2 rounded-full mx-1 cursor-pointer ${
+              index === activeIndex ? 'bg-white' : 'bg-gray-300'
+            }`}
+            onClick={() => handleIndicatorClick(index)}
+          ></div>
+        ))}
       </div>
     </div>
   );
