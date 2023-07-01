0d13c6590efefc7ebb46a444df126668a8655e59
Helitha Rupasinghe <hr21don>
2023-07-01 09:58:52 +0100

    first commit 🔥

---
+++ data.js
@@ -0,0 +1,60 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "/wizard.png",
+        health: 100,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    elf: {
+        name: "Elf",
+        avatar: "/elf.png",
+        health: 40,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Orc",
+        avatar: "/orc.png",
+        health: 5,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "/demon.png",
+        health: 10,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "/goblin.png",
+        health: 15,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    skeleton: {
+        name: "Skeleton",
+        avatar: "/skeleton.png",
+        health: 20,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    troll: {
+        name: "Troll",
+        avatar: "/troll.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: {}
+    },
+    vampire: {
+        name: "Vampire",
+        avatar: "/vampire.png",
+        health: 30,
+        diceCount: 3,
+        currentDiceScore: {}
+    }
+}
+
+export default characterData
---
+++ main.js
@@ -0,0 +1,79 @@
+import characterData from "./data.js";
+import Character from "./Character.js";
+
+let monstersArray = ["orc", "demon", "goblin", "skeleton", "troll", "vampire"];
+let heroArray = ["hero", "elf"];
+let isWaiting = false;
+
+function getNewMonster() {
+  const nextMonsterData = characterData[monstersArray.shift()];
+  return nextMonsterData ? new Character(nextMonsterData) : {};
+}
+
+function getNewHero() {
+  const nextHeroData = characterData[heroArray.shift()];
+  return nextHeroData ? new Character(nextHeroData) : {};
+}
+
+function attack() {
+  if (!isWaiting) {
+    wizard.setDiceHtml();
+    monster.setDiceHtml();
+    wizard.takeDamage(monster.currentDiceScore);
+    monster.takeDamage(wizard.currentDiceScore);
+    render();
+
+    if (wizard.dead) {
+      // wizard = getNewHero(); // Replace the wizard with the elf
+      // render();
+      endGame();
+    } else if (monster.dead) {
+      isWaiting = true;
+      if (monstersArray.length > 0) {
+        setTimeout(() => {
+          monster = getNewMonster();
+          render();
+          isWaiting = false;
+        }, 1500);
+      } else {
+        endGame();
+      }
+    }
+  }
+}
+
+function endGame() {
+  isWaiting = true;
+  const endMessage =
+    wizard.health === 0 && monster.health === 0
+      ? "No victors - all creatures are dead"
+      : wizard.health > 0
+      ? "The Wizard Wins"
+      : "The monsters are Victorious";
+
+  const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
+  setTimeout(() => {
+    document.body.innerHTML = `
+                <div class="end-game">
+                    <h2>Game Over</h2> 
+                    <h3>${endMessage}</h3>
+                    <p class="end-emoji">${endEmoji}</p>
+                </div>
+                `;
+  }, 1500);
+}
+
+document.getElementById("attack-button").addEventListener("click", attack);
+
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = monster.getCharacterHtml();
+}
+
+// const wizard = new Character(characterData.hero)
+let wizard = getNewHero();
+let monster = getNewMonster();
+render();
+
+
+		
