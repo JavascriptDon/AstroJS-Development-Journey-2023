2d040e792be3d2828615e9256a8933d1a920c53b
Helitha Rupasinghe <hr21don>
2023-07-02 11:29:18 +0100

    first commit👨‍🚀

---
+++ Dog.js
@@ -0,0 +1,30 @@
+class Dog {
+    constructor(data) {
+        Object.assign(this, data);
+    }
+
+    getDogHtml() {
+        const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this
+        return `
+            <img class="dog-img" src=${avatar}>
+            <div class="dog-info">
+                <h1 class="name-age">${name}, ${age}</h1>
+                <p class="dog-bio">${bio}</p>
+            </div>
+            
+            `;
+    }
+
+   noMoreDogs() {
+        document.getElementById("card").innerHTML = `
+            <div class="no-dogs">
+                <h1>🐶⁉️</h1>
+                <h2>There are no more dogs in your area</h2>
+                <p>Click the paw logo to try again</p>
+            </div>
+        `;
+        document.getElementById("btn-container").classList.add("hide");
+    }
+}
+
+export default Dog;
---
+++ index.js
@@ -0,0 +1,46 @@
+// Remember to import the data and Dog class!
+import dogs from '/data.js'
+import Dog from '/Dog.js'
+
+const acceptBtnEl = document.getElementById('acceptBtnEl')
+const rejectBtnEl = document.getElementById('rejectBtnEl')
+
+
+const dog = new Dog(dogs.shift())
+document.getElementById('card').innerHTML = dog.getDogHtml()
+
+acceptBtnEl.addEventListener("click", function(){
+    dog.hasBeenLiked = true
+    if (!acceptBtnEl.disabled) {
+    document.getElementById("accept-badge").style.display = "block";
+    acceptBtnEl.classList.add("accept-btn-clicked")
+    selectionMade()}
+})
+
+rejectBtnEl.addEventListener("click", function(){
+    if (!rejectBtnEl.disabled) {
+    document.getElementById("reject-badge").style.display = "block";
+    rejectBtnEl.classList.add("reject-btn-clicked")
+    selectionMade()}
+})
+
+function selectionMade() {
+    rejectBtnEl.disabled = true
+    acceptBtnEl.disabled = true
+    dog.hasBeenSwiped = true
+    if (dogs.length > 0) {
+        setTimeout(function(){
+            const dog = new Dog(dogs.shift())
+            document.getElementById('card').innerHTML = dog.getDogHtml()
+            dog.hasBeenSwiped = false;
+            document.getElementById("accept-badge").style.display = "none";
+            document.getElementById("reject-badge").style.display = "none";
+            acceptBtnEl.classList.remove("heart-btn-clicked");
+            rejectBtnEl.classList.remove("cross-btn-clicked");
+            rejectBtnEl.disabled = false
+            acceptBtnEl.disabled = false
+            }, 2000)
+    }else {
+        dog.noMoreDogs();
+    }
+}
